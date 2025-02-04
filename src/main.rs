9e9a68ae0d2e227f110a961c29bba5051022fdc8
Reid Herrera <reidherrerabusiness@gmail.com>
2025-02-04 10:58:52 -0600

    created a bill calculator using meal structs.

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,34 @@
-fn main() {}
+#[derive(Debug)]
+struct FoodBill {
+    meal: String,
+    price: f64,
+    dessert: bool,
+    total: f64,
+}
+
+impl FoodBill {
+    fn new(meal: String, price: f64, dessert: bool, total: f64) -> Self {
+        Self {
+            meal,
+            price,
+            dessert,
+            total,
+        }
+    }
+
+    fn total_bill_calculator(&mut self) -> f64 {
+        if self.dessert == false {
+            self.total = (self.price * 0.2) + self.price;
+            self.total
+        } else {
+            self.total = (self.price * 0.2) + self.price + 5.00;
+            self.total
+        }
+    }
+}
+
+fn main() {
+    let mut meal_one: FoodBill = FoodBill::new(String::from("Chicken Alfredo"), 32.00, true, 0.00);
+
+    println!("{:#?}", meal_one.total_bill_calculator())
+}
