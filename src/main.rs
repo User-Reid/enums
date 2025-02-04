bcbc82616a03abcfce4444ca05774318049d7873
Reid Herrera <reidherrerabusiness@gmail.com>
2025-02-04 15:08:07 -0600

    understanding associated values when dealing with Enums. You can store multiple associated values within one or more enum types, and how you can impliment them into your variables and mutate them.

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,16 @@
 #[derive(Debug)]
-enum CardSuit {
-    Hearts,
-    Diamonds,
-    Spades,
-    Clubs,
-}
-
-struct Card {
-    rank: String,
-    suit: CardSuit,
+enum PaymentMethodType {
+    CreditCard(String),
+    DebitCard(String),
+    PayPal(String, String),
 }
 
 fn main() {
-    let first_card: CardSuit = CardSuit::Hearts;
-    let mut second_card: CardSuit = CardSuit::Spades;
-    let third_card: CardSuit = CardSuit::Clubs;
-    second_card = CardSuit::Diamonds;
+    let mut my_payment_method: PaymentMethodType =
+        PaymentMethodType::CreditCard(String::from("1322-3123-3232"));
 
-    let card_suits: [CardSuit; 2] = [CardSuit::Hearts, CardSuit::Clubs];
-    let card_suits2: (CardSuit, CardSuit) = (CardSuit::Hearts, CardSuit::Clubs);
+    my_payment_method =
+        PaymentMethodType::PayPal(String::from("Bob@gmail.com"), String::from("Tacobell!"));
 
-    println!("{:#?}", second_card)
+    println!("{:#?}", my_payment_method)
 }
